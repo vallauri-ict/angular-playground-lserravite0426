ff76ef0bedab478d73d4718160dd921076b141c3
lserravite0426 <57641140+lserravite0426@users.noreply.github.com>
2020-03-04 16:00:19 +0100

    Angular course

---
+++ angular-course/src/app/server-farm/server-farm.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-farm',
+  templateUrl: './server-farm.component.html',
+  styleUrls: ['./server-farm.component.css']
+})
+export class ServerFarmComponent implements OnInit {
+
+  servers = [];
+  serverName: string = '';
+  serverCreated: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.servers.push('');
+    this.serverCreated = true;
+  }
+
+  onChangeServerName() {
+    this.serverCreated = false;
+  }
+
+}
